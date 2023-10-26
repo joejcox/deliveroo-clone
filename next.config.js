216fccd176e5235cc0bb54c15ee93b6a1953630c
joejcox <joecox@live.co.uk>
2023-10-26 23:12:53 +0100

    chore: remove incorrect code from next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [{
-      hostname: "https://consumer-component-library.roocdn.com"
-  }]
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
